8ce5cf279e058a10fde1e2afd4f7321fb3d6a025
snsasaki <sn.sasaki@core-tech.jp>
2026-06-19 14:48:18 +0900

    :horse: 初期データ挿入

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,26 @@ import type { Contact } from "./types/Contact";
 import ContactItem from "./components/ContactItem";
 
 function App() {
-  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [contacts, setContacts] = useState<Contact[]>([
+    {
+      id: 1,
+      title: "ログインできない",
+      detail: "パスワードを忘れました",
+      status: "pending",
+    },
+    {
+      id: 2,
+      title: "画面が表示されない",
+      detail: "トップページが真っ白です",
+      status: "in_progress",
+    },
+    {
+      id: 3,
+      title: "問い合わせ完了",
+      detail: "解決済みです",
+      status: "completed",
+    },
+  ]);
 
   const handleAdd = (contact: Omit<Contact, "id">) => {
     const newContact: Contact = {
