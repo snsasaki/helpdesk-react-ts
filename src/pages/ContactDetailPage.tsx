fcebdfd1c2ecf969f21c572ff52ca79d4e746d37
snsasaki <sn.sasaki@core-tech.jp>
2026-06-19 16:33:13 +0900

    :sparkles: 詳細（編集）画面への遷移機能

--- src/pages/ContactDetailPage.tsx
+++ src/pages/ContactDetailPage.tsx
@@ -1,7 +1,23 @@
-function ContactDetailPage() {
+import type { Contact } from "../types/Contact";
+
+type Props = {
+  contact: Contact;
+  onBack: () => void;
+};
+
+function ContactDetailPage({ contact }: Props) {
+  const statusLabels = {
+    pending: "未対応",
+    in_progress: "対応中",
+    completed: "完了",
+  };
   return (
     <>
+      {/* TODO: 情報がデフォルト入力されているフォームが表示される */}
       <h1>詳細ページです</h1>
+      <p>{contact.title}</p>
+      <p>{contact.detail}</p>
+      <p>{statusLabels[contact.status]}</p>
     </>
   );
 }
