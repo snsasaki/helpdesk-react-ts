689b912a77ad62e8e4f82bac902cd72c47815d36
snsasaki <sn.sasaki@core-tech.jp>
2026-06-19 13:31:54 +0900

    :sparkles: Formコンポーネントを用意

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import ContactForm from "./components/ContactForm";
+
 function App() {
   return (
     <>
       <h1>ヘルプデスク</h1>
+      <ContactForm />
     </>
   );
 }
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,14 @@
+function ContactForm() {
+  return (
+    <form>
+      <input
+        // value={text}
+        // onChange={handleChange}
+        placeholder="お問い合わせ内容を入力してください"
+      />
+      <button type="submit">追加</button>
+    </form>
+  );
+}
+
+export default ContactForm;
